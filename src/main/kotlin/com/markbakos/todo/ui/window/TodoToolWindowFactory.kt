d318c4762c1a8a3196fb14afbdf9295465cbe308
markbakosss <markbakosss@gmail.com>
2025-06-26 15:58:15 +0200

    moved TodoToolWindowFactory.kt

--- src/main/kotlin/com/markbakos/todo/ui/window/TodoToolWindowFactory.kt
+++ src/main/kotlin/com/markbakos/todo/ui/window/TodoToolWindowFactory.kt
@@ -1,13 +1,12 @@
-package com.markbakos.todo.ui
+package com.markbakos.todo.ui.window
 
-import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 import com.markbakos.todo.ui.controller.TodoManagerPanel
 import org.jetbrains.annotations.NotNull
 import javax.swing.JComponent
-
 
 class TodoToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(@NotNull project: Project, @NotNull toolWindow: ToolWindow) {
