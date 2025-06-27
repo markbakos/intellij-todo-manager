6c5266885044708d81a6c2b5d71f86973e262752
markbakosss <markbakosss@gmail.com>
2025-06-27 17:08:03 +0200

    added saving and loading logic to settings manager

--- src/main/kotlin/com/markbakos/todo/ui/controller/SettingsManager.kt
+++ src/main/kotlin/com/markbakos/todo/ui/controller/SettingsManager.kt
@@ -1,18 +1,26 @@
 package com.markbakos.todo.ui.controller
 
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.project.Project
 
 // Manager class for handling settings
 class SettingsManager private constructor(private val project: Project) {
     companion object {
         private val instances = mutableMapOf<Project, SettingsManager>()
+        private const val LANGUAGE_KEY = "todo.plugin.language"
+        private const val DEFAULT_LANGUAGE = "en"
 
         fun getInstance(project: Project): SettingsManager {
-            return instances.getOrPut(project) { SettingsManager(project) }
+            return instances.getOrPut(project) {
+                SettingsManager(project).apply {
+                    loadSettings()
+                }
+            }
         }
     }
 
-    private var currentLanguage: String = "en" //default english
+    private var currentLanguage: String = DEFAULT_LANGUAGE //default english
+    private val propertiesComponent: PropertiesComponent = PropertiesComponent.getInstance(project)
 
     fun getCurrentLanguage(): String = currentLanguage
 
@@ -26,10 +34,10 @@ class SettingsManager private constructor(private val project: Project) {
     }
 
     fun saveSettings() {
-        // TODO: implement saving settings logic to PropertiesCOmponent
+        propertiesComponent.setValue(LANGUAGE_KEY, currentLanguage)
     }
 
     fun loadSettings() {
-        // TODO: implement settings loading
+        currentLanguage = propertiesComponent.getValue(LANGUAGE_KEY, DEFAULT_LANGUAGE)
     }
 }
